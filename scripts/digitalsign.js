097ee8a13878c8a6e6406aed4593d1d4d6a8236c
damianpetroff <damian.petroff@he-arc.ch>
2019-05-05 22:03:21 +0200

    Final commit before delivery. Change the main demo to be Alice-Bob tranfer etc

--- scripts/digitalsign.js
+++ scripts/digitalsign.js
@@ -11,13 +11,70 @@ document.addEventListener("DOMContentLoaded", function() {
     // Only enable the cryptographic operation buttons if a key pair can be created
     document.getElementById("sign").addEventListener("click", signTheFile);
     document.getElementById("verify").addEventListener("click", verifyTheFile);
-    document.getElementById("openPGPGenerateKeys").addEventListener("click", function()
+
+    //Demo buttons
+    document.getElementById("openPGPDemo_generatekeys").addEventListener("click", function()
     {keyPair = createAndSaveAKeyPair().
     catch(function(err) {
         alert("Could not create a keyPair or enable buttons: " + err.message + "\n" + err.stack);
     });});
-    document.getElementById("openPGPTest").addEventListener("click", test);
+    document.getElementById("openPGPDemo_sign").addEventListener("click", demo_sign);
+    document.getElementById("openPGPDemo_transfer").addEventListener("click", demo_transfer);
+    document.getElementById("openPGPDemo_verify").addEventListener("click", demo_verify);
 
+    async function demo_sign()
+    {
+      document.getElementById("openpgp_sign_waiting").hidden = false;
+      var msg = document.getElementById("raw_message_openpgp").value;
+      var cleartext;
+      var privkey = keyPair.privateKeyArmored;
+      var passphrase = 'super long and hard to guess secret';
+      var privKeyObj = (await openpgp.key.readArmored(privkey)).keys[0];
+      await privKeyObj.decrypt(passphrase);
+      var options_sign = {
+        message: openpgp.cleartext.fromText(msg),
+        privateKeys: [privKeyObj]
+      };
+      await openpgp.sign(options_sign).then(function(signed) {
+          cleartext = signed.data;
+      });
+      document.getElementById("signed_message_a_openpgp").innerHTML = cleartext;
+      document.getElementById("openpgp_sign_waiting").hidden = true;
+      document.getElementById("openPGPDemo_transfer").disabled = false;
+    }
+
+    function demo_transfer(){
+      document.getElementById("openpgp_transfer_waiting").hidden = false;
+      document.getElementById("signed_message_b_openpgp").innerHTML = document.getElementById("signed_message_a_openpgp").value;
+      document.getElementById("openpgp_transfer_waiting").hidden = true;
+      document.getElementById("openPGPDemo_verify").disabled = false;
+
+    }
+
+    async function demo_verify()
+    {
+      document.getElementById("openpgp_verify_waiting").hidden = false;
+      var cleartext = document.getElementById("signed_message_b_openpgp").value;
+      var pubkey = keyPair.publicKeyArmored;
+      var validity;
+      var options_verify = {
+          message: await openpgp.cleartext.readArmored(cleartext), // parse armored message
+          publicKeys: (await openpgp.key.readArmored(pubkey)).keys // for verification
+      };
+      await openpgp.verify(options_verify).then(function(verified) {
+        validity = verified.signatures[0].valid; // true
+        if (validity) {
+          document.getElementById("openpgp_demo_result").innerHTML = "<span class=\"badge badge-success\">Success : The author of the message is garanteed to be Alice.</span>";
+        }
+        else {
+          document.getElementById("openpgp_demo_result").innerHTML = "<span class=\"badge badge-danger\">Failed</span>";
+        }
+      });
+      document.getElementById("openpgp_verify_waiting").hidden = true;
+    }
+
+
+    document.getElementById("openPGPTest").addEventListener("click", test);
     async function test()
     {
       document.getElementById("openpgp_test_waiting").hidden = false;
@@ -76,6 +133,7 @@ document.addEventListener("DOMContentLoaded", function() {
             document.getElementById("verify").disabled = false;
             var revocationCertificate = key.revocationCertificate; // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
             document.getElementById("openpgp_genkey_waiting").hidden = true;
+            document.getElementById("openPGPDemo_sign").disabled = false;
             keyPair = key;
             return key;
         });
@@ -89,6 +147,8 @@ document.addEventListener("DOMContentLoaded", function() {
         // Click handler. Reads the selected file, then signs it to
         // the random key pair's private key. Creates a Blob with the result,
         // and places a link to that Blob in the download-results section.
+        document.getElementById("openpgp_file_waiting").hidden = false;
+        document.getElementById("openpgp_file_result").innerHTML = "";
         var sourceFile = document.getElementById("source-file").files[0];
 
         var reader = new FileReader();
@@ -150,9 +210,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
             } // End of sign
         } // end of processTheFile
+        document.getElementById("openpgp_file_waiting").hidden = true;
     } // end of signTheFile click handler
-
-
 
 
     function verifyTheFile() {
@@ -161,7 +220,7 @@ document.addEventListener("DOMContentLoaded", function() {
         // saying whether the signature is valid or not. If the signature is
         // valid, it also creates a Blob with the original file
         // and places a link to that Blob in the download-results section.
-
+        document.getElementById("openpgp_file_waiting").hidden = false;
         var sourceFile = document.getElementById("source-file").files[0];
 
         var reader = new FileReader();
@@ -178,9 +237,9 @@ document.addEventListener("DOMContentLoaded", function() {
             verify(data, keyPair.publicKeyArmored).
             then(function(blob) { // Paul: peut virer, cf code de la doc (y'a une ligne if(validity){...})
                 if (blob === null) {
-                    alert("Invalid signature!");
+                    document.getElementById("openpgp_file_result").innerHTML = "<span class=\"badge badge-danger\">Fail : The signature is not valid.</span><br>";
                 } else {
-                    alert("Signature is valid.");
+                    document.getElementById("openpgp_file_result").innerHTML = "<span class=\"badge badge-success\">Success : The signature is valid.</span><br>";
                     var url = URL.createObjectURL(blob);
                     document.getElementById("download-links").insertAdjacentHTML(
                         'beforeEnd',
@@ -222,6 +281,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
             } // end of verify
         } // end of processTheFile
+        document.getElementById("openpgp_file_waiting").hidden = true;
     } // end of decryptTheFile
 
 });
